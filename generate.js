a70eb71df6e58ef5a92304feb229998976ab5c9e
Olivia Asch <olivia.c.asch@gmail.com>
2020-12-30 13:58:30 +0000

    Create canvas and draw start position

--- generate.js
+++ generate.js
@@ -1,7 +1,12 @@
 let random;
 
+let canvas;
+let ctx;
+
 let width;
 let height;
+
+let stitchSize;
 
 let x;
 let y;
@@ -10,9 +15,18 @@ function generate() {
   const enteredWidth = parseInt(document.getElementById('width').value);
   const enteredHeight = parseInt(document.getElementById('height').value);
 
-  width = !enteredWidth || isNaN(enteredWidth) ? 300 : Math.abs(enteredWidth);
+  width =
+    !enteredWidth || isNaN(enteredWidth) ? 300 : Math.abs(enteredWidth) + 1;
   height =
-    !enteredHeight || isNaN(enteredHeight) ? 200 : Math.abs(enteredHeight);
+    !enteredHeight || isNaN(enteredHeight) ? 200 : Math.abs(enteredHeight) + 1;
+
+  canvas = document.getElementById('canvas');
+
+  stitchSize = calculateStitchSize();
+  canvas.width = width * stitchSize;
+  canvas.height = height * stitchSize;
+
+  ctx = canvas.getContext('2d');
 
   const userSeed = document.getElementById('seed-input').value;
   const timeSeed = new Date().getTime().toString(36);
@@ -20,9 +34,14 @@ function generate() {
   updateSeedDisplay(seed);
   random = new Math.seedrandom(seed);
 
+  drawBorder();
+
   const { startX, startY } = chooseStartPosition();
   x = startX;
   y = startY;
+
+  displayStartPosition();
+  drawStitch();
 
   printState();
 }
@@ -54,11 +73,41 @@ function chooseStartPosition() {
   return { startX, startY };
 }
 
+function displayStartPosition() {
+  const startPositionDisplay = document.getElementById('start-position');
+  startPositionDisplay.innerText = `Start position: (${x + 1}, ${y + 1})`;
+  startPositionDisplay.classList.remove('hidden');
+}
+
+function drawBorder() {
+  ctx.strokeStyle = '#cccccc';
+  ctx.strokeRect(0, 0, width * stitchSize, height * stitchSize);
+}
+
+function drawStitch() {
+  ctx.fillStyle = '#0e82e8';
+  ctx.fillRect(x * stitchSize, y * stitchSize, stitchSize, stitchSize);
+}
+
+function drawStartPosition() {
+  ctx.fillStyle = '#000000';
+  ctx.fillText(x, x * stitchSize, y * stitchSize);
+}
+
+function calculateStitchSize() {
+  const { width: availableWidth } = document
+    .getElementById('root')
+    .getBoundingClientRect();
+
+  return Math.floor(availableWidth / width);
+}
+
 function printState() {
   console.log({
     width,
     height,
     x,
     y,
+    stitchSize,
   });
 }
